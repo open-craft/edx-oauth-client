0d29e07e843da9f4c3922b8c7d121aea7efed548
Oksana Slusarenro <oksana.slu@gmail.com>
2017-12-07 11:33:59 +0200

    Add save fields country, profile_image

--- edx_oauth_client/pipeline.py
+++ edx_oauth_client/pipeline.py
@@ -1,27 +1,23 @@
-import string  # pylint: disable-msg=deprecated-module
-import json
-import logging
 import requests
+import urllib2
 
 from cms.djangoapps.course_creators.models import CourseCreator
-from django.http import HttpResponseBadRequest, HttpResponse
+from django.http import HttpResponseBadRequest
 from django.contrib.auth.models import User
+from openedx.core.djangoapps.profile_images.images import create_profile_images
+from openedx.core.djangoapps.profile_images.views import _make_upload_dt, LOG_MESSAGE_CREATE
+from openedx.core.djangoapps.user_api.accounts.image_helpers import get_profile_image_names, set_has_profile_image
 
 from social.pipeline import partial
 from django_countries import countries
 
 from student.views import create_account_with_params, reactivation_email_for_user
-from student.models import UserProfile, CourseAccessRole
-from student.roles import (
-    CourseInstructorRole, CourseStaffRole, GlobalStaff, OrgStaffRole,
-    UserBasedRole, CourseCreatorRole, CourseBetaTesterRole, OrgInstructorRole,
-    LibraryUserRole, OrgLibraryUserRole
-)
+from student.models import UserProfile
+
 from third_party_auth.pipeline import (
     make_random_password, AuthEntryError
 )
 
-from opaque_keys.edx.keys import CourseKey
 from logging import getLogger
 
 log = getLogger(__name__)
@@ -63,15 +59,16 @@ def ensure_user_information(
             resp = requests.get(api, headers=headers)
             country = resp.json()['data'][0]['country']
             log.info('Get country from API: %s', country)
-            country = dict(map(lambda x: (x[1], x[0]), countries)).get(country, country)
+        country = dict(map(lambda x: (x[1], x[0]), countries)).get(country, country)
 
-        data['username'] = user_data['username']
+        data['username'] = clean_username(user_data['username'])
         data['first_name'] = user_data['firstName']
         data['last_name'] = user_data['lastName']
         data['email'] = user_data['email']
         data['country'] = country
         data['access_token'] = access_token
         data['name'] = data['first_name'] + " " + data['last_name']
+        profile_image = user_data.get('profileImage') or user_data.get('coverImage')
     except Exception as e:
         log.error('Exception %s', e)
         raise AuthEntryError(backend, 'can\' get user data.')
@@ -122,18 +119,26 @@ def ensure_user_information(
             user.save()
             CourseCreator.objects.get_or_create(user=user)
 
-        try:
-            user_profile = UserProfile.objects.get(user=user)
-        except User.DoesNotExist:
-            user_profile = None
-        except User.MultipleObjectsReturned:
-            user_profile = UserProfile.objects.filter(user=user)[0]
-
-        if user_profile:
-            user_profile.name = user.get_full_name()
-            user_profile.save()
-
     user = user or response.get('user')
+
+    try:
+        user_profile = UserProfile.objects.get(user=user)
+    except User.DoesNotExist:
+        user_profile = None
+    except User.MultipleObjectsReturned:
+        user_profile = UserProfile.objects.filter(user=user)[0]
+
+    if user_profile:
+        user_profile.name = user.get_full_name()
+
+        if not user_profile.country:
+            user_profile.country = data['country']
+
+        user_profile.save()
+
+    if profile_image and user_profile and not user_profile.has_profile_image:
+        upload_profile_image(profile_image, user.username)
+
     if user and not user.is_active:
         if allow_inactive_user:
             pass
@@ -142,3 +147,32 @@ def ensure_user_information(
             raise AuthEntryError(backend, user.email)
 
     return {'user': user}
+
+
+def clean_username(username):
+    username = username.replace('', '_')
+    prefix = 1
+
+    while True:
+        if User.objects.filter(username=username).exists():
+            username += str(prefix)
+            prefix += 1
+        else:
+            break
+
+    return username
+
+
+def upload_profile_image(profile_image, username):
+    uploaded_file = urllib2.urlopen(profile_image)
+    # generate profile pic and thumbnails and store them
+    profile_image_names = get_profile_image_names(username)
+    create_profile_images(uploaded_file, profile_image_names)
+
+    # update the user account to reflect that a profile image is available.
+    set_has_profile_image(username, True, _make_upload_dt())
+
+    log.info(
+        LOG_MESSAGE_CREATE,
+        {'image_names': profile_image_names.values(), 'user_id': username}
+    )
