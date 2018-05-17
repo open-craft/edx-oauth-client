baa44d87b053996846b505be20d11499575e9304
Yura [flying_pi] Braiko <fp.flyingpi@gmail.com>
2018-05-17 19:58:14 +0300

    fix incorrect import (#9)

--- edx_oauth_client/middleware.py
+++ edx_oauth_client/middleware.py
@@ -6,7 +6,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth import REDIRECT_FIELD_NAME, logout
 from django.shortcuts import redirect
 
-from social.apps.django_app.views import auth, NAMESPACE
+from social_django.views import auth, NAMESPACE
 
 try:
     from opaque_keys.edx.keys import CourseKey
