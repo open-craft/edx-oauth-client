d51aa825921e05865d461a8e8c9933d428cd242a
Serhii Mandziuk <serhii.mandziuk@raccoongang.com>
2019-08-16 16:22:06 +0300

    LETSTUDY-56_Add_aditional_no_redirect_local_url

--- edx_oauth_client/middleware.py
+++ edx_oauth_client/middleware.py
@@ -92,7 +92,7 @@ class OAuthRedirection(object):
             'i18n', 'search', 'verify_student', 'certificates', 'jsi18n', 'course_modes', '404', '500', 'i18n.js',
             'wiki', 'notify', 'courses', 'xblock', 'change_setting', 'account', 'notification_prefs', 'admin',
             'survey', 'event', 'instructor_task_status', 'edinsights_service', 'openassessment', 'instructor_report',
-            'logout', 'dashboard'
+            'logout', 'dashboard', 'course_run', 'change_enrollment', 'change_email_settings',
         )
 
         handle_local_urls += auth_process_urls + api_urls
